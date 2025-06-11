78ce637c386c03e50440ea1722f09c2dd60e78d5
Franchesco Gallegos <carlos.gallegos03@epn.edu.ec>
2025-06-10 19:05:42 -0500

    Añadiendo navbar al proyecto y su logica de negocios

--- src/main/java/madstodolist/controller/HomeController.java
+++ src/main/java/madstodolist/controller/HomeController.java
@@ -1,14 +1,30 @@
 package madstodolist.controller;
 
+import madstodolist.authentication.ManagerUserSession;
+import madstodolist.dto.UsuarioData;
+import madstodolist.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Autowired
+    ManagerUserSession managerUserSession;
 
     @GetMapping("/about")
     public String about(Model model) {
+
+        Long idUsuarioLogeado = managerUserSession.usuarioLogeado();
+        if (idUsuarioLogeado != null) {
+            UsuarioData usuario = usuarioService.findById(idUsuarioLogeado);
+            model.addAttribute("nombreUsuario", usuario.getNombre());
+        }
+        model.addAttribute("idUsuarioLogeado", idUsuarioLogeado);
         return "about";
     }
 
