52d407e20ca35c26396f837a32e242d8e6ac454b
Franchesco Gallegos <carlos.gallegos03@epn.edu.ec>
2025-06-10 20:10:31 -0500

    Lista De Usuarios controlador y pagina html

--- src/main/java/madstodolist/service/UsuarioService.java
+++ src/main/java/madstodolist/service/UsuarioService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -71,5 +72,12 @@ public class UsuarioService {
         else {
             return modelMapper.map(usuario, UsuarioData.class);
         }
+    }
+
+
+
+    @Transactional(readOnly = true)
+    public List<Usuario> findAllUsers() {
+        return (List<Usuario>) usuarioRepository.findAll();
     }
 }
